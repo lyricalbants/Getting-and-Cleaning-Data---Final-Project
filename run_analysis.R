7cb03d7bd0b68dfda85901291c1b18e42a74b503
lyricalbants <emmanuel.ezeobidi@yahoo.com>
2019-07-12 09:34:20 +0100

    Done!

--- run_analysis.R
+++ run_analysis.R
@@ -24,17 +24,16 @@ list.files('./data/UCI HAR Dataset/test/Inertial Signals')
 
 ## open the activity_label file
 activity <- read_csv('./data/UCI HAR Dataset/activity_labels.txt', col_names = FALSE)
-#activity <- fread('./data/UCI HAR Dataset/activity_labels.txt')
-#activity <- as_tibble(activity)
 activity <- separate(activity, X1, into = c("index", "activity"), sep = " ")
 
 ## open the features text file
-feature <- read_csv('./data/UCI HAR Dataset/features.txt', col_names = FALSE)
-feature <- separate(feature, X1, into = c("index", "features"), sep = " ")
+#feature <- read_csv('./data/UCI HAR Dataset/features.txt', col_names = FALSE)
+#feature <- separate(feature, X1, into = c("index", "features"), sep = " ")
+feature <- fread('./data/UCI HAR Dataset/features.txt')
+names(feature) <- c("index", "features")
 
 ## open the subject_test text file in the test folder
-#sub_test <- as_tibble(fread('./data/UCI HAR Dataset/test/subject_test.txt'))
-sub_test <- read_csv('./data/UCI HAR Dataset/test/subject_test.txt', col_names = "id", 
+sub_test <- read_csv('./data/UCI HAR Dataset/test/subject_test.txt', col_names = "subject", 
                      col_types = cols(.default = col_factor()))
 
 ## open the X_test text file in the test folder
@@ -42,13 +41,14 @@ test_set <- read_delim('./data/UCI HAR Dataset/test/X_test.txt',
                        col_names = feature$features, delim = " ", col_types = cols(.default = col_number())
 )
 test_set <- as_tibble(test_set, .name_repair = "universal")
+names(test_set) <- tolower(names(test_set))
 ## extract mean and sd of features in the test dataset
 test_ms <- bind_cols(select(test_set, contains("mean")), select(test_set, contains("std")))
 
 
 ## open the y_test text file in the test folder
 y_test <- read_csv('./data/UCI HAR Dataset/test/y_test.txt', 
-                   col_names = "labels")
+                   col_names = "activity")
 
 ## create factor variables, test_labels
 test_labels <- as_tibble(sapply(y_test, factor, levels = activity$index, labels = activity$activity))
@@ -65,7 +65,7 @@ list.files('./data/UCI HAR Dataset/train')
 list.files('./data/UCI HAR Dataset/train/Inertial Signals')
 
 ## open the subject_train text file in the train folder
-sub_train <- read_csv('./data/UCI HAR Dataset/train/subject_train.txt', col_names = "id",
+sub_train <- read_csv('./data/UCI HAR Dataset/train/subject_train.txt', col_names = "subject",
                       col_types = cols(.default = col_factor()))
 
 ## open the x_train text file in the train folder
@@ -73,12 +73,13 @@ train_set <- read_delim('./data/UCI HAR Dataset/train/X_train.txt',
                         col_names = feature$features, delim = " ", col_types = cols(.default = col_number())
 )
 train_set <- as_tibble(train_set, .name_repair = "universal")
+names(train_set) <- tolower(names(train_set))
 ## extract mean and sd of features in the train dataset
 train_ms <- bind_cols(select(train_set, contains("mean")), select(train_set, contains("std")))
 
 
 ## open the y_train text file in the test folder
-y_train <- read_csv('./data/UCI HAR Dataset/train/y_train.txt', col_names = "labels")
+y_train <- read_csv('./data/UCI HAR Dataset/train/y_train.txt', col_names = "activity")
 
 ## create factor variables, train_labels
 train_labels <- as_tibble(sapply(y_train, factor, levels = activity$index, labels = activity$activity))
@@ -91,8 +92,8 @@ data <- right_join(df, df1)
 
 # -------------------------------------------------------------------#
 # create a different dataset by using id and labels columns as factor variables.
-factoredData <- group_by(data, id, labels)
-tidydata <- summarize_each(factoredData, mean, (tBodyAcc.mean...X:fBodyBodyGyroJerkMag.std..))
+factoredData <- group_by(data, subject, activity)
+tidydata <- summarize_each(factoredData, mean, (tbodyacc.mean...x:fbodybodygyrojerkmag.std..))
 
 # Export tidydata as text file
 write_delim(tidydata, "tidy.txt")
